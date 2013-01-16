6d866366489983ad9d23baa12f6f28df032a8ebd
Yaroslav Geryatovich <ygeryatovich@cogniance.com>
2013-01-16 16:52:06 +0200

    first reactions works, but with reactive lib as jar

---
+++ reactive-android/src/main/scala/Lib.scala
@@ -0,0 +1,7 @@
+import android.util.Log
+
+object Lib {
+  
+  def printIt = Log.d("scala", "yep, it's library")
+
+}
--- reactive-android/src/main/scala/ReactiveDemo.scala
+++ reactive-android/src/main/scala/ReactiveDemo.scala
@@ -2,12 +2,19 @@ package com.yarikx.reactiveandroid
 
 import _root_.android.app.Activity
 import _root_.android.os.Bundle
+import reactive.EventSource
+import android.util.Log
+import reactive.Observing
 
-class ReactiveDemo extends Activity with TypedActivity {
+class ReactiveDemo extends Activity with TypedActivity with Observing{
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
 
-    findView(TR.textview).setText("hello, world!")
+    findView(TR.textview).setText("hello, scala world is hard, not for everyone")
+    
+    val q = new EventSource[String];
+    q.foreach(x => Log.d("reactive",x))
+    q.fire("wow")
   }
 }
