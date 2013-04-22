96d592dd9ed3746cf86f3af75efa08c5fc7b2733
Yaroslav Geryatovich <ygeryatovich@cogniance.com>
2013-04-22 17:26:19 +0300

    renamed some helper classes

--- reactive-android/src/main/scala/org/yarikx/reactiveandroid/util/Helpers.scala
+++ reactive-android/src/main/scala/org/yarikx/reactiveandroid/util/Helpers.scala
@@ -6,13 +6,13 @@ import android.widget.{Button, CheckBox, EditText, ProgressBar, SeekBar, TextVie
 import org.yarikx.reactiveandroid.model.ReactiveClicker
 import org.yarikx.reactiveandroid.view.{ReactiveCheckBox, ReactiveEditText, ReactiveProgressBar, ReactiveSeekbar, ReactiveTextView}
 
-class ReactiveButtonHelper(context: Context, attrs: AttributeSet) extends Button(context, attrs) with ReactiveClicker
+class RButton(context: Context, attrs: AttributeSet) extends Button(context, attrs) with ReactiveClicker
 
 class RSeekbar(context: Context, attrs: AttributeSet) extends SeekBar(context, attrs) with ReactiveSeekbar
 
-class ReactiveTextViewHelper(context: Context, attrs: AttributeSet) extends TextView(context, attrs) with ReactiveTextView
+class RTextView(context: Context, attrs: AttributeSet) extends TextView(context, attrs) with ReactiveTextView
 
-class ReactiveEditTextHelper(context: Context, attrs: AttributeSet) extends EditText(context, attrs) with ReactiveEditText
+class REditText(context: Context, attrs: AttributeSet) extends EditText(context, attrs) with ReactiveEditText
 
 class RProgressBar(context: Context, attrs: AttributeSet) extends ProgressBar(context, attrs) with ReactiveProgressBar
 
